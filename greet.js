1d3e16705ed5f8ad16a522a929dbe4705318d79d
ludwej <jacobsludwe84@gmail.com>
2018-05-22 10:35:27 +0200

    greet html

---
+++ greet.js
@@ -0,0 +1,28 @@
+var GreetBtn = document.querySelector(".GreetBtn");
+var ResetBtn = document.querySelector(".ResetBtn");
+var text = document.querySelector(".Text");
+var lang = document.querySelector(".lang");
+
+ var name = '' ;
+ // var greet = {} ;
+
+GreetBtn.addEventListener('click' ,greeT)
+// ResetBtn.addEvent)(Listener
+
+function greeT(){
+  //To get the bill type to add from the radio button
+  var checkedRadioBtn = document.querySelector("input[name='lang']:checked");
+  if (checkedRadioBtn){
+      var checked = checkedRadioBtn.value;
+
+
+  }
+
+
+function greet() {
+      if (language === Name) {
+        greet = 'Hello ,' + name ;
+          console.log(language);
+      }
+
+}
